7e48e7b0f34c44414ad487d7035bebf4900b5105
Rus Ioan <rus.io.ioan@student.utcluj.ro>
2024-03-25 17:49:48 +0200

    What i did int LAB session 3

---
+++ src/Lab4/App2/Main.java
@@ -0,0 +1,10 @@
+package Lab4.App2;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer monitor = new Integer(1);
+        Integer monitor2 = new Integer(1);
+        new ExecutionThread(monitor,monitor, 4, 4, 2, 4,4,6).start();
+        new ExecutionThread(monitor2,monitor, 5, 5, 3, 5,5,7).start();
+    }
+}
